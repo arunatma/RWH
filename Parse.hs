ed338d1735de5bfb5e49e0a6a8b80663714e4130
Arunram Atmacharan <arunram.atma@gmail.com>
2017-02-06 21:20:01 +0530

    Ch12: Barcode;  Introduction to Array datatype

---
+++ Parse.hs
@@ -0,0 +1,415 @@
+-- Real World Haskell
+
+module Parse where 
+-- only the needed subset taken from 
+-- Chapter 10: Code case study: parsing a binary data format
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import Data.Char (isSpace, chr, isDigit)
+import Data.Int (Int64)
+import Data.Word (Word8)
+
+data Greymap = Greymap {
+      greyWidth :: Int
+    , greyHeight :: Int
+    , greyMax :: Int
+    , greyData :: L.ByteString
+    } deriving (Eq)
+    
+-- greyData may contain huge content. default 'show' makes to display a string 
+-- which is unusually huge in size. So, do not derive Show typeclass
+-- Similarly no point in deriving Read - or writing a Read instance
+
+-- Writing our own instance of Show for Greymap
+instance Show Greymap where
+    show (Greymap w h m _) = "Greymap " ++ show w ++ "x" ++ show h ++
+                             " " ++ show m
+
+data ParseState = ParseState {
+      string :: L.ByteString
+    , offset :: Int64
+    } deriving (Show)
+    
+    
+-- with this change to Algebraic Data Type; both residual string and the offset 
+
+simpleParse :: ParseState -> (a, ParseState)
+simpleParse = undefined
+
+-- to help with error
+betterParse :: ParseState -> Either String (a, ParseState)
+betterParse = undefined
+
+-- Future proof the code: Do not expose the implementation details
+-- Wrap it with "newtype" wrapper 
+
+newtype Parse a = Parse {
+    runParse :: ParseState -> Either String (a, ParseState)
+}
+-- usage of record syntax here
+-- runParse is the accessor function auotmatically derived
+-- runParse :: Parse a -> (ParseState -> Either String (a, ParseState)
+-- runParse gets a Parse data type and returns a function that can take a 
+-- ParseState and returns Either String (a, ParseState)
+
+-- identity parser (gives whatever is input as the return of the parser)
+identity :: a -> Parse a
+identity a = Parse (\s -> Right(a, s))
+
+-- "Parse" value constructor takes in a function - that function takes in a 
+-- ParseState and returns a Either data type
+-- Here, the identity parser creates a 'Parse' to which any state is passed
+-- returns with the same state 's'.  The argument given as input is used in 
+-- the result 'a'
+
+-- Using the wrapped function (ParseState -> Either (a, ParseState)
+-- 'parse' function
+parse :: Parse a -> L.ByteString -> Either String a
+parse parser initState
+    = case runParse parser (ParseState initState 0) of 
+        Left err            -> Left err
+        Right (result, _)   -> Right result
+        
+-- 'parse' or 'identity' functions do not even inspect what the state 's' is!
+
+-- Using the record syntax for modifying offset
+modifyOffset :: ParseState -> Int64 -> ParseState
+modifyOffset initState newOffset = initState {offset = newOffset}
+
+{-
+    ghci> let before = ParseState (L8.pack "foo") 0
+    ghci> let after = modifyOffset before 3
+    ghci> before
+    ParseState {string = Chunk "foo" Empty, offset = 0}
+    ghci> after
+    ParseState {string = Chunk "foo" Empty, offset = 3}
+-}        
+
+-- Parser to parse a single byte
+
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+    case L.uncons (string initState) of
+        Nothing                 -> bail "no more input"
+        Just (byte, remainder)  -> 
+            putState newState ==> \_ -> identity byte
+            where
+                newState  = initState {string = remainder, offset = newOffset}
+                newOffset = offset initState + 1
+                
+{-
+    L8.uncons takes the first element from the ByteString and presents the 
+    remainder
+    
+    ghci> L8.uncons (L8.pack "foo")
+    Just ('f',Chunk "oo" Empty)
+    ghci> L8.uncons L8.empty
+    Nothing
+    
+    getState function retrieves the current parsing state
+    putState function replaces the parsing state
+    bail     function terminates and reports an error
+-}                
+
+-- getState and putState functions
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+-- The left element of the tuple is the result of a Parse (ref. by a usually), 
+-- The right is the current ParseState (s). 
+
+-- getState fn extracts the current parsing state so we can access to the
+-- string and the offset
+-- putState fn replaces the current ParseState with a new one taken as input
+
+-- Need not pass tuples around now
+-- Can get done with the use of getState and putState functions
+
+-- Details of the parsing state now in ParseState
+-- Instead of pattern matching, we now use the accessor functions 
+-- Now if new info is needed in the ParseState, we can change the data type,
+-- add new info with its accessor and only those new functions that need the 
+-- new info will use the new accessor. No touching the existing fns which are
+-- using the other info from ParseState.
+-- If we had used pattern matching, all code needs to be retouched to handle 
+-- the new info in ParseState
+
+-- Two more functions to be defined which are in parseByte function
+-- one is "bail" and another is "(==>)"
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
+
+-- (==>) chains the parsers together
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+    where chainedParser initState = 
+            case runParse firstParser initState of
+                Left errMessage               -> 
+                    Left errMessage
+                Right (firstResult, newState) -> 
+                    runParse (secondParser firstResult) newState
+                
+
+-- Parse type is a wrapper to a function
+-- (==>) combines two such parse functions - Parse, take out the value, pass on
+-- the value derived from first to second parser.
+
+-- Functors
+-- Revisiting map function
+plus1 = map (+1) [1, 2, 3]
+showMe = map show [1, 2, 3]
+
+-- map like function in binary tree
+data Tree a = Node (Tree a) (Tree a) 
+            | Leaf a
+            deriving (Show)
+            
+-- let us assume the tree contain strings (Tree String)
+-- to get the length of strings in each of the node (again in the form of Tree)
+treeLengths :: Foldable t => Tree (t a) -> Tree Int
+treeLengths (Leaf s) = Leaf (length s)
+treeLengths (Node l r) = Node (treeLengths l) (treeLengths r)
+
+-- what if instead of finding the lengths, we need to prefix 'a' to each?
+-- Abstraction helps
+treeMap :: (a -> b) -> Tree a -> Tree b
+treeMap f (Leaf a)   = Leaf (f a)
+treeMap f (Node l r) = Node (treeMap f l) (treeMap f r)
+
+-- now treeLengths fn in terms of treeMap is:
+treeLen :: Tree [Char] -> Tree Int
+treeLen = treeMap length
+
+-- to prefix with 'a'
+treePrefix :: Tree [Char] -> Tree [Char]
+treePrefix = treeMap ('a':)
+{-
+    ghci> let tree = Node (Leaf "foo") (Node (Leaf "x") (Leaf "quux"))
+    ghci> treeLen tree
+    Node (Leaf 3) (Node (Leaf 1) (Leaf 4))
+
+    ghci> treePrefix tree
+    Node (Leaf "afoo") (Node (Leaf "ax") (Leaf "aquux"))
+    
+    ghci> treeMap (even . length) (treePrefix tree)
+    Node (Leaf True) (Node (Leaf True) (Leaf False))
+    
+-}
+
+-- Haskell has a type-class to generalise treeMap
+{-
+    class Functor f where
+        fmap :: (a -> b) -> f a -> f b
+-}    
+
+-- Any data type to want to be an instance of the Functor type-class should
+-- implement the 'fmap' function.  
+
+-- fmap is a lifting function.  Lifts the function from the plain context to 
+-- that of the container context
+-- fmap :: (a -> b) -> (f a -> f b)
+-- another way to look at fmap:  Take a function that operates on plain values
+-- Give me another function that does the same on contextual values
+
+-- making Tree an instance of a Functor
+instance Functor Tree where
+    fmap = treeMap
+
+-- making list [] an instance of a Functor (already done in GHC.Base)
+{-    
+    instance Functor [] where
+        fmap = map    
+-}    
+
+-- making Maybe an instance of a Functor (already done in GHC.Base)
+{-    
+    instance Functor Maybe where
+        fmap _ Nothing = Nothing
+        fmap f (Just x) = Just (f x)
+-}    
+
+-- We can make Functor instances for datatypes with only one parameter
+-- We cannot do it for Either a b or (a, b)
+
+-- Also no constraint on data type definition
+
+-- Valid Functor
+data Foo a = Foo a
+           
+instance Functor Foo where
+    fmap f (Foo a) = Foo (f a)
+    
+
+-- Invalid to write a Functor instance on Bar, as it has a constraint
+{-
+    data Eq a => Bar a = Bar a
+    
+    instance Functor Bar where
+        fmap f (Bar a) = Bar (f a)
+-}
+-- Better have the constraints on the function definitions
+
+-- Adding a constraint to a type definition is never a good idea. 
+-- That forces you to add type constraints to each function that operates on 
+-- the values of that type. This is deprecated in the latest versions
+{- 
+    data (Ord a) => OrdStack a = Bottom
+                               | Item a (OrdStack a)
+                                 deriving (Show)
+-}
+
+-- infix use of fmap
+fmapEx1 = (1+) `fmap` [2,3,4] ++ [5,6,7]    -- [3,4,5,5,6,7]
+fmapEx2 = (1+) `fmap` ([2,3,4] ++ [5,6,7])  -- [3,4,5,6,7,8]
+fmapEx3 = (1+) <$> ([2,3,4] ++ [5,6,7])     -- [3,4,5,6,7,8]
+-- (<$>) is nothing but fmap infix operator defined in Data.Functor
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+
+--Flexible Instances
+--------------------
+-- We cannot create a functor for a data type with more than one variable, but
+-- we can do so it we make all others concrete expcept for one
+-- Not possible to create Functer for Either 
+-- But, it is possible to create functor instance for Either a
+
+{-
+Already defined in GHC.Base.
+To define this we need to use the Language Pragma Flexible Instances
+
+    instance Functor (Either a) where
+        fmap g (Right x) = Right (g x)
+        fmap g (Left x) = Left x    
+-}
+
+-- Functor Laws
+lhsFunctorLaw1 = id (Node (Leaf "a") (Leaf "b"))
+rhsFunctorLaw1 = fmap id (Node (Leaf "a") (Leaf "b"))
+--  Law 1: fmap id == id 
+
+lhsFunctorLaw2 = (fmap even . fmap length) (Just "twelve")
+rhsFunctorLaw2 = fmap (even . length) (Just "twelve")
+--  Law 2: fmap (f . g) == fmap f . fmap g
+
+-- Functor instance for Parse data type
+-- The function that we are 'fmap'ping should be applied to the current result
+-- of the parse, with the parse state untouched. So, use the 'identity' function
+-- and map the function 'f' only on the result.
+
+instance Functor Parse where
+    fmap f parser = parser ==> \result -> 
+                    identity (f result)
+
+-- any such functor instance should follow the 2 functor laws
+-- 1st law:
+plain = parse parseByte L.empty           -- Left "byte offset 0: no more input"
+withId = parse (id <$> parseByte) L.empty -- Left "byte offset 0: no more input"
+
+inWord = L8.pack "foo"
+plain2 = parse parseByte inWord           -- Right 102
+withId2 = parse (id <$> parseByte) inWord -- Right 102
+
+-- 2nd law: (Composability)
+lhsParseLaw2 = parse ((chr . fromIntegral) <$> parseByte) inWord
+rhsParseLaw2 = parse (chr <$> fromIntegral <$> parseByte) inWord
+
+-- Using functors for parsing
+-- Let us say we want to write the parseChar function similar to parseByte
+-- We need not duplicate the same, and make use of fmap
+-- The functor takes the result of the parse and applies a function (we can
+-- as well convert from Byte to Char)
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+-- Using functors to "peek"
+-- Look at what the byte is without actually consuming it
+-- Nothing when at the end of the string
+-- remember 'string' is an accessor function in ParseState definition
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
+-- here also two fmap lifting (using fmap and <$>), due to Parse and Maybe
+
+-- Similarly we can write a peekChar using the fmap lifting
+peekChar :: Parse (Maybe Char)
+peekChar = fmap w2c <$> peekByte    -- Two lifting because of Parse and Maybe
+
+-- writing parseWhile, analogous to takeWhile
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte) ==> \mp ->
+               if mp == Just True
+               then parseByte ==> \b ->
+                    (b:) <$> parseWhile p
+               else identity []
+
+-- same function, without using functors
+parseWhileVerbose :: (Word8 -> Bool) -> Parse [Word8]
+parseWhileVerbose p =
+    peekByte ==> \mc ->
+    case mc of
+        Nothing -> identity []
+        Just c | p c ->
+                   parseByte ==> \b ->
+                   parseWhileVerbose p ==> \bs ->
+                   identity (b:bs)
+               | otherwise -> 
+                   identity []
+                   
+-- Now, rewriting the PGM Parser
+parseRawPGM = 
+    parseWhileWith w2c notWhite ==> \header -> skipSpaces ==>&
+    assert (header == "P5") "invalid raw header" ==>&
+    parseNat ==> \width -> skipSpaces ==>&
+    parseNat ==> \height -> skipSpaces ==>&
+    parseNat ==> \maxGrey ->
+    parseByte ==>&
+    parseBytes (width * height) ==> \bitmap ->
+    identity (Greymap width height maxGrey bitmap)
+  where notWhite = (`notElem` "\r\n\t")
+  
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
+
+parseNat :: Parse Int
+parseNat = parseWhileWith w2c isDigit ==> \digits ->
+           if null digits
+           then bail "no more input"
+           else let n = read digits
+                in if n < 0
+                   then bail "integer overflow"
+                   else identity n
+
+-- This ==>& is same as ==>, except that right hand side ignores the result of
+-- the left.  So one parsing done, followed by another without looking what the
+-- first parse has given as result                   
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> \_ -> f
+
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+
+assert :: Bool -> String -> Parse ()
+assert True _ = identity ()
+assert False err = bail err
+
+-- parseP5 kept passing the tuples 
+-- parseRawPGM has kept the parsing state completely hidden
+
+-- inspecting and modifying the parsing state is done using parseBytes function
+parseBytes :: Int -> Parse L.ByteString
+parseBytes n =
+    getState ==> \st ->
+    let n' = fromIntegral n
+        (h, t) = L.splitAt n' (string st)
+        st' = st { offset = offset st + L.length h, string = t }
+    in putState st' ==>&
+       assert (L.length h == n') "end of input" ==>&
+       identity h
--- ch12.hs
+++ ch12.hs
@@ -10,6 +10,25 @@
 -- 3rd Group: 5 digits - Product Id (assigned by manufacturer)
 -- 4th Group: 1 digit - check digit - to validate the scan
 
+
+import Data.Array (Array(..), (!), bounds, elems, indices,
+                   ixmap, listArray)
+
+import Control.Applicative ((<$>))
+import Control.Monad (forM_)
+import Data.Char (digitToInt)
+import Data.Ix (Ix(..))
+import Data.List (foldl', group, sort, sortBy, tails)
+import Data.Maybe (catMaybes, listToMaybe)
+import Data.Ratio (Ratio)
+import Data.Word (Word8)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Map as M
+
+import Parse                            -- from chapter 10
+
+
 -- To calculate check digit:
 -- Take the first 12 digits, multiply all even digits by 3. Add them together
 -- Add the result with the sum of odd digits. Divide by 10. Find reminder.
@@ -30,4 +49,111 @@ mapEveryOther f = zipWith ($) (cycle [f, id])
 
 -- Separate encodings for the six digits on the left and on the right
 -- Parity bits encode the left group and the 13th digit.
+
+-- Here, to get the random access in the list, we will be using "Array"
+-- In bar code parsing, we use table look-ups
+-- such table looks ups is difficult to achieve with lists or tuples
+-- As list needs to be traversed linearly and tuple needs pattern matching
+
+-- For constant time random access:  Array data structure
+-- So, represent encoding tables as array of strings
+
+leftOddList = ["0001101", "0011001", "0010011", "0111101", "0100011",
+               "0110001", "0101111", "0111011", "0110111", "0001011"]
+
+rightList = map complement <$> leftOddList
+    where complement '0' = '1'
+          complement '1' = '0'
+
+leftEvenList = map reverse rightList
+
+parityList = ["111111", "110100", "110010", "110001", "101100",
+              "100110", "100011", "101010", "101001", "100101"]
+
+listToArray :: [a] -> Array Int a
+listToArray xs = listArray (0,l-1) xs
+    where l = length xs
+
+leftOddCodes, leftEvenCodes, rightCodes, parityCodes :: Array Int String
+
+leftOddCodes = listToArray leftOddList
+leftEvenCodes = listToArray leftEvenList
+rightCodes = listToArray rightList
+parityCodes = listToArray parityList
+
+-- Array has a special feature
+-- One dimensional array of string will be of type Arry Int String
+-- 2d array of string will be of type Array (Int, Int) String
+
+-- array indices can be higher than the number of elements
+-- But, if the list to be made array is of lesser length than array index,
+-- exception thrown for the higher indices for which values from list not avbl
+indexExceedsList = listArray (0,3) [True,False,False,True,False]
+listExceedsIndex = listArray (0,10) "too short"
+-- no error until the index 9 or 10 is accessed here
+{-
+    *Main> listExceedsIndex ! 8
+    't'
+    *Main> listExceedsIndex ! 9
+    *** Exception: (Array.!): undefined array element
+    *Main> listExceedsIndex ! 10
+    *** Exception: (Array.!): undefined array element
+    *Main> listExceedsIndex ! 11
+    *** Exception: Ix{Integer}.index: Index (11) out of range ((0,10))
+-}    
+-- So, Arrays are lazy in evaluation
+
+-- access the element using (!) operator
+alphaArray = listArray (0,25) ['A'..]
+letterZ = alphaArray ! 25
+letterBeyond = alphaArray ! 100
+
+-- the bounds need not start with 0
+modulo7 = listArray (-3,3) [4,5..] 
+reminder4 = modulo7 ! (-3)
+reminder3 = modulo7 ! (3)
+
+-- the bounds can be any member of 'Ix' type 
+asciiVals = listArray ('A', 'Z') [65..]
+asciiA = asciiVals ! 'A'
+asciiZ = asciiVals ! 'Z'
+
+-- multi dimensional array 
+threeDimArray = listArray ((0,0,0), (9,9,9)) [0..]
+elem654 = threeDimArray ! (6, 5, 4)
+
+-- multi dimensional with multiple index types
+twoDimArray = listArray ((0, 'a'), (9, 'z')) [0..]
+elem9c = twoDimArray ! (9, 'c')
+
+-- Folding over Arrays
+-- bounds and indices function
+-- bounds give the tuple used in creating the Array.
+-- indices expands that tuple as a list.
+{-
+    *Main> bounds listExceedsIndex
+    (0,10)
+    *Main> indices listExceedsIndex
+    [0,1,2,3,4,5,6,7,8,9,10]
+-}
+
+-- So, (range . bounds) === indices
+
+-- No inbuilt fold functions in Data.Array module
+-- Strict left fold similar to foldl' on lists.
+foldA :: Ix k => (a -> b -> a) -> a -> Array k b -> a
+foldA f s a = go s (indices a)
+    where go s (j:js) = let s' = f s (a ! j)
+                        in s' `seq` go s' (js)
+          go s _ = s
+          
+-- Strict left fold (with first element of array as starting value)
+-- similar to foldl1 on lists.
+foldA1 :: Ix k => (a -> a -> a) -> Array k a -> a
+foldA1 f a = foldA f (a ! fst (bounds a)) a
+
+-- Why there is no default fold in Arrays?
+-- fold is possible only in one dimension. On two dimension, row-wise and 
+-- column-wise options come and there are multiple ways to think of a fold 
+-- over n-dimensional data.
 
