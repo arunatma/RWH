42c528643281d6a637f1b4482f66584520ec8bac
Arunram Atmacharan <arunram.atma@gmail.com>
2017-03-02 15:17:07 +0530

    Units functionality added - separate instances have to be difined for Num, Show, Fractional and Floating

--- num.hs
+++ num.hs
@@ -120,3 +120,80 @@ simplify (BinaryArith op ia ib) =
 simplify (UnaryArith op a) = UnaryArith op (simplify a)
 simplify x = x
 
+-- Support for Units
+
+-- Defining a new data type for Units
+data Units a = Units a (SymbolicManip a) deriving (Eq)
+-- Units contain a number and a label (the label is of SymbolicManip type)
+
+instance (Eq a, Num a) => Num (Units a) where
+    (Units xa ua) + (Units xb ub)
+        | ua == ub = Units (xa + xb) ua
+        | otherwise = error "Mis-matched units in add or subtract"
+    (Units xa ua) - (Units xb ub) = (Units xa ua) + (Units (xb * (-1)) ub)
+    (Units xa ua) * (Units xb ub) = Units (xa * xb) (ua * ub)
+    negate (Units xa ua) = Units (negate xa) ua
+    abs (Units xa ua) = Units (abs xa) ua
+    signum (Units xa _) = Units (signum xa) (Number 1)
+    fromInteger i = Units (fromInteger i) (Number 1)
+    
+-- Making the units SymbolicManip instead of String solves the units handling
+-- Now m * m automatically becomes "m * m" - no need to explicitly do any work
+
+instance (Eq a, Fractional a) => Fractional (Units a) where
+    (Units xa ua) / (Units xb ub) = Units (xa / xb) (ua / ub)
+    recip a = 1 / a
+    fromRational r = Units (fromRational r) (Number 1)
+    
+instance (Eq a, Floating a) => Floating (Units a) where
+    pi = Units pi (Number 1)
+    exp _ = error "exp is not implemented in units"
+    log _ = error "log is not implemented in units"
+    (Units xa ua) ** (Units xb ub)
+        | ub == Number 1 = Units (xa ** xb) (ua ** Number xb)
+        | otherwise = error "units for RHS of ** not supported"
+    sqrt (Units xa ua) = Units (sqrt xa) (sqrt ua)
+    sin (Units xa ua)
+        | ua == Symbol "rad" = Units (sin xa) (Number 1)
+        | ua == Symbol "deg" = Units (sin (deg2rad xa)) (Number 1)
+        | otherwise          = error "Units for sin must be deg or rad"
+    cos (Units xa ua)
+        | ua == Symbol "rad" = Units (cos xa) (Number 1)
+        | ua == Symbol "deg" = Units (cos (deg2rad xa)) (Number 1)
+        | otherwise          = error "Units for cos must be deg or rad"
+    tan (Units xa ua)
+        | ua == Symbol "rad" = Units (tan xa) (Number 1)
+        | ua == Symbol "deg" = Units (tan (deg2rad xa)) (Number 1)
+        | otherwise          = error "Units for tan must be deg or rad"
+    asin (Units xa ua) 
+        | ua == Number 1 = Units (rad2deg $ asin xa) (Symbol "deg")
+        | otherwise = error "Units for asin must be empty"
+    acos (Units xa ua)
+        | ua == Number 1 = Units (rad2deg $ acos xa) (Symbol "deg")
+        | otherwise = error "Units for acos must be empty"
+    atan (Units xa ua)
+        | ua == Number 1 = Units (rad2deg $ atan xa) (Symbol "deg")
+        | otherwise = error "Units for atan must be empty"
+    sinh = error "sinh not yet implemented in Units"
+    cosh = error "cosh not yet implemented in Units"
+    tanh = error "tanh not yet implemented in Units"
+    asinh = error "asinh not yet implemented in Units"
+    acosh = error "acosh not yet implemented in Units"
+    atanh = error "atanh not yet implemented in Units"
+    
+-- Degree Radian conversions
+deg2rad x = 2 * pi * x / 360
+rad2deg x = 360 * x / (2 * pi)    
+
+-- take a number and a string and return the same in "Units" data type
+units :: (Num z) => z -> String -> Units z
+units a b = Units a (Symbol b)
+
+-- reverse converstion from Units to plain number
+dropUnits :: (Num z) => Units z -> z
+dropUnits (Units x _) = x
+
+-- Show instance for Units
+instance (Show a, Num a, Eq a) => Show (Units a) where
+    show (Units xa ua) = show xa ++ "::" ++ prettyShow (simplify ua)
+    
