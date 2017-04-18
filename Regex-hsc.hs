293812f07ab94182b3869ce826c1b9fd33c8a6f7
Arunram Atmacharan <arunram.atma@gmail.com>
2017-04-18 21:01:04 +0530

    Writing a PCRE Compile equivalent in Haskell

--- Regex-hsc.hs
+++ Regex-hsc.hs
@@ -18,7 +18,7 @@ import Foreign.C.Types
 newtype PCREOption = PCREOption { unPCREOption :: CInt }
     deriving (Eq,Show)
     
--- PCRE Constants
+-- PCRE Constants (Compile options)
 caseless       :: PCREOption
 caseless       = PCREOption #const PCRE_CASELESS
 
@@ -30,3 +30,44 @@ dotall         = PCREOption #const PCRE_DOTALL
     
 -- Use hsc2hs 
 -- C> hsc2hs Regex-hsc.hs to convert this .hsc file to .hs file     
+
+-- PCRE Constants can either be defined manually like above
+-- Or, we can use automatic binding as below
+-- PCRE Compile Options
+#{enum PCREOption, PCREOption
+  , caseless1       = PCRE_CASELESS
+  , dollar_endonly1 = PCRE_DOLLAR_ENDONLY
+  , dotall          = PCRE_DOTALL
+  }
+
+-- #enum has 3 fields
+-- First field: PCREOption (how the haskell type to be treated as)
+-- Second field: PCREOption (Optional constructor)
+-- Third field: Name and constant association
+
+-- Here we shall treat flags as abstract types (not as bitfields in C)
+-- So, or-ing or and-ing of flags will be treated differently in Haskell
+
+-- | Combine a list of options into a single option, using bitwise (.|.)
+-- Take out the value from the PCREOption using unPCREOption and do bitwise OR
+combineOptions :: [PCREOption] -> PCREOption
+combineOptions = PCREOption . foldr ((.|.) . unPCREOption) 0
+
+-- This is how pcre_compile is defined in pcre.h
+{-
+    pcre *pcre_compile(const char *pattern,
+                       int options,
+                       const char **errptr,
+                       int *erroffset,
+                       const unsigned char *tableptr);
+-}
+-- compiles a regular expression into an internal format 
+
+-- The equivalent data types are defined in Foreign.C.Types
+-- Argument descriptions:
+-- 1st: Null '\0' terminated character pointer (for regex pattern)
+--    : Haskell Equivalent is CString
+-- 2nd: Options (int)
+--    : We shall use PCREOption (runtime representation is CInt)
+-- 3rd: a pointer to a C String - reference to any error msg during regex 
+--      compilation 
--- Regex-hsc_out.hs
+++ Regex-hsc_out.hs
@@ -1,14 +1,10 @@
 {-# LINE 1 "Regex-hsc.hs" #-}
 {-# LANGUAGE CPP, ForeignFunctionInterface #-}
 {-# LINE 2 "Regex-hsc.hs" #-}
-
 -- Real World Haskell
 -- Chapter 17: Foreign Function Interface
 -- http://book.realworldhaskell.org/read/interfacing-with-c-the-ffi.html
 
--- This is a generated file using hsc2hs 
--- This can be loaded into ghci (valid and will get compiled)
--- ghci> :l Regex-hsc_out.hs
 -- Part of Chapter 17
 
 module Regex where
@@ -25,7 +21,7 @@ import Foreign.C.Types
 newtype PCREOption = PCREOption { unPCREOption :: CInt }
     deriving (Eq,Show)
     
--- PCRE Constants
+-- PCRE Constants (Compile options)
 caseless       :: PCREOption
 caseless       = PCREOption 1
 {-# LINE 24 "Regex-hsc.hs" #-}
@@ -40,3 +36,15 @@ dotall         = PCREOption 4
     
 -- Use hsc2hs 
 -- C> hsc2hs Regex-hsc.hs to convert this .hsc file to .hs file     
+
+-- PCRE Constants can either be defined manually like above
+-- Or, we can use automatic binding as below
+-- PCRE Compile Options
+caseless1        :: PCREOption
+caseless1        = PCREOption 1
+dollar_endonly1  :: PCREOption
+dollar_endonly1  = PCREOption 32
+dotall           :: PCREOption
+dotall           = PCREOption 4
+
+{-# LINE 42 "Regex-hsc.hs" #-}
